0a4c99369fd20572ec3cdfeb25a92bb9b394d09a
kumaravel v <75235334+Kumaravel655@users.noreply.github.com>
2022-04-26 19:21:51 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/androidlifecyclehelloworld/MainActivity.java
+++ app/src/main/java/com/example/androidlifecyclehelloworld/MainActivity.java
@@ -4,11 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast=Toast.makeText(getApplicationContext(),"OnCreate Executed",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnStart Executed",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnResume Executed",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnPause Executed",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnStop Executed",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnRestart Executed",Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast toast=Toast.makeText(getApplicationContext(),"OnDestroy Executed",Toast.LENGTH_LONG);
+        toast.show();
     }
 }
